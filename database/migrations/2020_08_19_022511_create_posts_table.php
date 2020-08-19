7b442825917b91441548c8027ddf317678229dca
long <basturlong@gmail.com>
2020-08-19 09:55:26 +0700

    create table in feature/migrate

---
+++ database/migrations/2020_08_19_022511_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('category_id');
+            $table->string('auther_id');
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->string('content');
+            $table->string('status');
+            $table->integer('views');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
